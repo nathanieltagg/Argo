c8934c1ff80990cd2bbdc8bac2d023bcf40eaaad
tagg <tagg@f519fff5-c35d-4d71-ac75-d58c7fa56e9f>
2013-02-10 06:07:40 +0000

    almost working!
    git-svn-id: svn+ssh://neutrino.otterbein.edu/data/Repositories/Arachne/Argo/trunk@1090 f519fff5-c35d-4d71-ac75-d58c7fa56e9f
    
    Former-commit-id: 9784f3784aebdd988dd480bbed027455bcada479

---
+++ libs/MOTD.js
@@ -0,0 +1,25 @@
+//
+// Code for the Arachne Event Display
+// Author: Nathaniel Tagg ntagg@otterbein.edu
+// 
+// Licence: this code is free for non-commertial use. Note other licences may apply to 3rd-party code.
+// Any use of this code must include attribution to Nathaniel Tagg at Otterbein University, but otherwise 
+// you're free to modify and use it as you like.
+//
+
+gMOTD = "<p><em>Message of the day:</em><br/>"
++"Recent (minor) changes:"
++"<ul>"
++"<li> Hit Info Window now shows Clusters."
++"<ul>"
++"<li> MC Digraph display: dynamically look at what the MC did. Very cool; may even be useful someday."
++"</li>"
++"<li> Added Minos Near Dectector to the MRI viewer.</br>--Curt Brown"
++"</li>"
++"<li> On request, added clusters to 3d View"
++"</li>"
++"<li> Added a color scale option to the big hitmap view."
++"</li>"
++"<li> Improved support for Retina devices: resolution doubling!"
++"</li>"
++"</ul></p>";
