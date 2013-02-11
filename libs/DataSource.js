528c5b1ed305073a51283dca45488ad751f1fed7
tagg <tagg@f519fff5-c35d-4d71-ac75-d58c7fa56e9f>
2013-02-11 01:59:52 +0000

    Making it useful as well as working.
    git-svn-id: svn+ssh://neutrino.otterbein.edu/data/Repositories/Arachne/Argo/trunk@1094 f519fff5-c35d-4d71-ac75-d58c7fa56e9f
    
    Former-commit-id: 3869cc27eefc505b2414d681a973a4f38890e304

---
+++ libs/DataSource.js
@@ -0,0 +1,80 @@
+
+$(function(){
+  new DataSource();
+});
+
+function DataSource()
+{
+  /// Object to track things in the DataSource portlet and environs
+  
+  //
+  // Bindings for DataSource controls.
+  
+  $('#inFilename')  .keydown(function(e){if (e.keyCode == 13) { QueryServer('fe'); }});
+  $('#inFeEntry')   .keydown(function(e){if (e.keyCode == 13) { QueryServer('fe'); }});
+  $('#go_fe').click(function(){QueryServer('fe'); return false;});
+  
+  gStateMachine.BindObj('recordChange',this,"NewRecord");  
+}
+
+DataSource.prototype.NewRecord = function()
+{
+  
+  
+  if(gUrlToThisEvent) gUrlToLastEvent = gUrlToThisEvent;
+  var baseurl = location.href.split('?')[0];
+  baseurl = baseurl.replace("live\.html","arachne.html");
+
+  gUrlToThisEvent = baseurl + "?"
+                    +"filename="+gFile
+                    +"&entry="+gEntry
+                    // +"&slice="+gCurrentSlice
+                    ;    
+  $('#link-to-this-event').html('<a href="'+gUrlToThisEvent+'">Link to this event</a>');
+  $('#email-this-event').html('<a href="mailto:ntagg@otterbein.edu?subject=Arachne Bug&body='+escape(gUrlToThisEvent)+'">Email this event (Bug Report)</a>');
+  
+  $('input.next-event').click(DoNextEvent);
+  $('input.prev-event').click(DoPrevEvent);
+}                                         
+
+
+function DoNextEvent()
+{
+  // Check bounds - are we about to hit end of file?
+  var n = gRecord.source.numEntriesInFile;
+  if(gEntry+1 >= n) {
+    $('#warning-dialog-message').html("You are at the last entry of the file/subrun. Can't advance.");
+    $( "#warning-dialog" ).dialog({
+          modal: true,
+          buttons: {
+            Ok: function() {$( this ).dialog( "close" );}
+          }
+    });
+  } else {
+      if(gLastQueryType=='fe') {
+        gEntry = gEntry+1;
+        $('.inEntry').val(gEntry)
+      }
+      QueryServer('last_query_type');
+  }
+}
+
+function DoPrevEvent()
+{  
+  if(gEntry <= 0) {
+    $('#warning-dialog-message').html("You are at the first entry of the file/subrun. Can't move back.");
+    $( "#warning-dialog" ).dialog({
+          modal: true,
+          buttons: {
+            Ok: function() {$( this ).dialog( "close" );}
+          }
+    });
+  } else {
+    if(gLastQueryType=='fe') {
+      gEntry = gEntry-1;
+      $('.inEntry').val(gEntry)
+    }
+    QueryServer('last_query_type');
+  }
+}
+
--- libs/argo.js
+++ libs/argo.js
@@ -48,13 +48,8 @@ function GetUrlParameter( name )
 
 
 $(function(){
-  //
-  // Bindings for run/subrun/gate input.
-  //
-  
-  $('#inFilename')  .keydown(function(e){if (e.keyCode == 13) { QueryServer('fe'); }});
-  $('#inFeEntry')   .keydown(function(e){if (e.keyCode == 13) { QueryServer('fe'); }});
-  $('#go_fe').click(function(){QueryServer('fe'); return false;});
+
+  // Initial load - read parameters on URL
 
   
   
@@ -80,104 +75,6 @@ $(function(){
 });
 
 
-///
-/// Code that sets up keyboard key equivalents.
-///
-$(function() {
-  $(window).keypress(function(event){
-    if($(event.target).is(":input")) return true;
-    if(event.ctrlKey || event.altKey || event.metaKey) return true; // Don't intercept control-N for new window.
-    console.log("keypress",event.which);
-    switch(event.which) {
-      // case 44: // ',' key
-      // case 60: // '<' key
-      //   // DoPrevSliceAndGate(); return false;
-      // case 46: // '.' key
-      // case 62: // '>' key
-      //   // DoNextSliceAndGate(); return false;
-      // case 49: DoSlice(1); return false;  // number keys
-      // case 50: DoSlice(2); return false;  // number keys
-      // case 51: DoSlice(3); return false;  // number keys
-      // case 52: DoSlice(4); return false;  // number keys
-      // case 53: DoSlice(5); return false;  // number keys
-      // case 54: DoSlice(6); return false;  // number keys
-      // case 55: DoSlice(7); return false;  // number keys
-      // case 56: DoSlice(8); return false;  // number keys
-      // case 57: DoSlice(9); return false;  // number keys
-      // case 111: // 'o' key
-      // case 48:  // '0' key
-      //   DoSlice(0); return false;  // '1' key
-      // case 97: DoSlice(-1); return false;  // 'a' key
-      // case 45: //'-'
-      // case 95: //'_'
-      //   DoSlicePrev(); return false;
-      // case 43: //'+'
-      // case 61: //'='
-      //   DoSliceNext(); return false;
-      // case 112: // 'p'
-      //   DoPrevEvent(); return false;
-      // case 110: // 'n'
-      //   DoNextEvent(); return false;
-      // 
-      // case 104: // 'h'
-      //   $('#ctl-show-hitmap-hits').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // case 98: // 'b'
-      //   $('#ctl-show-hitmap-blobs').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // case 99: // 'c'
-      //   $('#ctl-show-hitmap-clusters').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // case 118: // 'v'
-      //   $('#ctl-show-hitmap-vertices').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // case 116: // 't'
-      //   $('#ctl-show-hitmap-tracks').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // case 114: // 'r'
-      //   $('#ctl-show-hitmap-regions').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // case 109: // 'm'
-      //   $('#ctl-show-hitmap-truth').click(); gStateMachine.Trigger('phColorChange');
-      //   return false;
-      // 
-      // case 117: // 'u'
-      //   $('#ctl-show-hitmap-hits-user-color').click();
-      //   return false;
-      // 
-      // case 92: // '\'
-      //   // Cycle through MC interactions
-      //   var active = $('#mc-info .accordion').accordion('option', 'active');  
-      //   var count = $('#mc-info .accordion h3').size();
-      //   var next = (active+1)%count;
-      //   // console.log("active accordion: ",active,count,next);
-      //   $('#mc-info .accordion').accordion('activate', next);  
-      //   return false;
-      // 
-      // 
-      // case 105: // 'i'
-      //   if($('#hit-info').is(":hidden")) {
-      //     $('#hit-info').dialog('open').dialog('moveToTop').effect('highlight');
-      //   } else {
-      //     $('#hit-info').dialog('close');
-      //   }
-      //   return false;
-      // 
-      case 80:
-        // console.log("P:",event);
-        DoPrint(gPortletHover);
-        break;
-        
-      case 76:
-        // shift-L: print everything.
-        return DoPrint($('body'),true);
-        break;
-        
-      default:
-        return true;
-    }
-  });
-});
 
 ///
 /// Code that sets up collapsibles.
@@ -226,7 +123,6 @@ $(function(){
   });
   // Printable.
   
-  // This doesn't actually work. Returns all messed up.
   $('#Print').click(function(){return DoPrint($('#everything'),true);});
 
   $(".portlet-header .ui-icon-print").click(function(){    
