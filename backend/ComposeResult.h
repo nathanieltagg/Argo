6842337be8d1a8e3515761b98c98caf7d8c0ebad
tagg <tagg@f519fff5-c35d-4d71-ac75-d58c7fa56e9f>
2013-06-06 15:09:32 +0000

    the common stuff.
    git-svn-id: svn+ssh://neutrino.otterbein.edu/data/Repositories/Arachne/Argoom/trunk@1250 f519fff5-c35d-4d71-ac75-d58c7fa56e9f
    
    Former-commit-id: fb8560a178a6a956a01d7328064b660239b8d831

---
+++ backend/ComposeResult.h
@@ -0,0 +1,13 @@
+#ifndef COMPOSERESULT_H
+#define COMPOSERESULT_H
+
+#include <string>
+
+std::string ComposeResult(
+    const std::string& filename, 
+    const std::string& histname, 
+    const std::string& options);
+
+
+#endif /* end of include guard: COMPOSERESULT_H */
+
