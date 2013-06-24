89aa849eb00fb476477b187207cc3c302af54f9b
tagg <tagg@f519fff5-c35d-4d71-ac75-d58c7fa56e9f>
2013-06-24 19:32:50 +0000

    Add a new, fresh copy of core. These are all good changes, but backporting to other branches will be a pain, so keep 'em local for the time being.
    git-svn-id: svn+ssh://neutrino.otterbein.edu/data/Repositories/Arachne/Argoom/trunk@1274 f519fff5-c35d-4d71-ac75-d58c7fa56e9f
    
    Former-commit-id: 3b30b7d81befeac37bc797ab6490c2dcb2156a95

---
+++ core/ABoundObject.js
@@ -0,0 +1,72 @@
+//
+// Code for the Arachne Event Display
+// Author: Nathaniel Tagg ntagg@otterbein.edu
+// 
+// Licence: this code is free for non-commertial use. Note other licences may apply to 3rd-party code.
+// Any use of this code must include attribution to Nathaniel Tagg at Otterbein University, but otherwise 
+// you're free to modify and use it as you like.
+//
+
+///
+/// Boilerplate:  Javascript utilities for MINERvA event display, codenamed "Arachne"
+/// Nathaniel Tagg  - NTagg@otterbein.edu - June 2009
+///
+
+// This class is a core base class for
+// models that bind to views.
+// The default arguments are the element it's attaching to,
+// and any configurable options you want to add to the class.
+// The object pulls data from the data-options field of the object.
+
+var gUniqueIdCounter=100;
+
+function ABoundObject(element, options)
+{
+  ///
+  /// Constructor.
+  ///
+  if(!element) return; 
+  if($(element).length<1) {return;} // Jquery object with zero length used.
+  this.element = $(element).get(0); // Get first bound object;
+  this.UniqueId = gUniqueIdCounter++;
+
+  // Merge in the options.
+  $.extend(true,this,options);
+
+  // Merge in options from element
+  var element_settings = $(element).data('options');
+  if(element_settings) {
+    // console.log(element_settings, element_settings_obj);
+    $.extend(true,this,element_settings); // Change default settings by provided overrides.  
+  }
+  
+}
+
+//
+// Utility.
+// See : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+ 
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+ 
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+ 
+    return fBound;
+  };
+}
+
+
