fbc79a614f5684e043c13a905a7d49fdd4d64a31
Nathaniel Tagg <tagg@fnal.gov>
2013-08-06 14:21:01 -0400

    Add preliminary muon curves.
    Former-commit-id: 743910bca7a8f257f454f7b0e226ec53d0bcf62c

--- libs/dEdXTool.js
+++ libs/dEdXTool.js
@@ -40,7 +40,24 @@ UserTrackPoint.prototype.set_to_zoom_center = function ()
   this.tdc= tdc;
 }
 
+UserTrackPoint.prototype.set_view_zoomhint = function (plane, wire, tdc)
+{
+  // Move this point, using the global view as the hint.
+  // Easiest: adjust the other two views to match this one.
+  this.tdc = tdc;
+  var deltaWire = wire - this[plane];
+  var dwire = [0,0,0];
+  switch(plane) {
+    case 0: dwire = [ deltaWire        ,-deltaWire*kcos60, deltaWire*kcos60 ]; break;
+    case 1: dwire = [-deltaWire*kcos60 , deltaWire       , deltaWire*kcos60 ]; break;
+    case 2: dwire = [ deltaWire*kcos60 , deltaWire*kcos60, deltaWire        ]; break;
+  }
 
+  this[0] += dwire[0];
+  this[1] += dwire[1];
+  this[2] += dwire[2];
+  
+}
 UserTrackPoint.prototype.set_view = function (plane, wire, tdc)
 {
   // Move the view to suggest the zoom point.
@@ -48,38 +65,23 @@ UserTrackPoint.prototype.set_view = function (plane, wire, tdc)
   // TDC is easy.
   this.tdc = tdc;
 
-  if(this.lasttouch==null || this.lasttouch.length < 2) {
-    // Easiest: adjust the other two views to match this one.
-    var deltaWire = wire - this[plane];
-    var dwire = [0,0,0];
-    switch(plane) {
-      case 0: dwire = [ deltaWire        ,-deltaWire*kcos60, deltaWire*kcos60 ]; break;
-      case 1: dwire = [-deltaWire*kcos60 , deltaWire       , deltaWire*kcos60 ]; break;
-      case 2: dwire = [ deltaWire*kcos60 , deltaWire*kcos60, deltaWire        ]; break;
-    }
- 
-    this[0] += dwire[0];
-    this[1] += dwire[1];
-    this[2] += dwire[2];
-  } else {
-    // Attempt to keep this view consistent with the last view we worked in.
-    // Find wire crossing point for this wire and the last touched.
-    var lastplane = this.lasttouch[1];
-    if(lastplane == plane) lastplane = this.lasttouch[0];
+  // Attempt to keep this view consistent with the last view we worked in.
+  // Find wire crossing point for this wire and the last touched.
+  var lastplane = this.lasttouch[1];
+  if(lastplane == plane) lastplane = this.lasttouch[0];
 
-    var gw1 = gGeo.getWire(plane,wire);
-    var gw2 = gGeo.getWire(lastplane, this[lastplane]);
-    var xing = gGeo.wireCrossing(gw1,gw2);
-    var thirdplane = 0;
-    while(thirdplane==plane || thirdplane== lastplane) thirdplane++;
-    console.warn(plane,wire);
-    console.warn(lastplane, this[lastplane]);
-    console.warn(gw2,xing);
-    var newwire = gGeo.yzToWire(thirdplane,xing.y,xing.z);
-    console.warn(thirdplane,newwire);
-    this[plane] = wire;
-    this[thirdplane] = newwire;
-  }
+  var gw1 = gGeo.getWire(plane,wire);
+  var gw2 = gGeo.getWire(lastplane, this[lastplane]);
+  var xing = gGeo.wireCrossing(gw1,gw2);
+  var thirdplane = 0;
+  while(thirdplane==plane || thirdplane== lastplane) thirdplane++;
+  console.warn(plane,wire);
+  console.warn(lastplane, this[lastplane]);
+  console.warn(gw2,xing);
+  var newwire = gGeo.yzToWire(thirdplane,xing.y,xing.z);
+  console.warn(thirdplane,newwire);
+  this[plane] = wire;
+  this[thirdplane] = newwire;
   
   if(this.lasttouch[1] != plane) {
     this.lasttouch[0] = this.lasttouch[1];
@@ -102,15 +104,37 @@ UserTrack.prototype.set_default = function()
   var b = new UserTrackPoint();
   // Width of Y-view:
   var y_width = gZoomRegion.plane[2][1] - gZoomRegion.plane[2][0];
-  b.set_view(2, a[2]+y_width/3, a.tdc);
+  b.set_view_zoomhint(2, a[2]+y_width/3, a.tdc);
   this.points = [a,b];
 }
 
+UserTrack.prototype.get_costheta_z = function(wire)
+{
+  // find theta_z in this segment.
+  var xyz = [];
+  for(var i = 0; i< this.points.length; i++) {
+    var ywire = gGeo.getWire(2,this.points[i][2]);
+    var vwire = gGeo.getWire(1,this.points[i][1]);
+    var p = gGeo.wireCrossing(ywire,vwire);
+    p.x = gGeo.getXofTDC(0,this.points[i].tdc);
+    xyz[i]=p;    
+  }
+  // FIXME: relies on only 2 points. Could use wire to find the right segment.
+  var dx = xyz[1].x - xyz[0].x;
+  var dy = xyz[1].y - xyz[0].y;
+  var dz = xyz[1].z - xyz[0].z;  
+  console.warn("Getting XYZ of usertrack",xyz[0],xyz[1]);
+  var cosz = dz/Math.sqrt(dx*dx + dy*dy + dz*dz);
+  return cosz;
+}
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// dEdX tool
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// Subclass of ABoundObject.
+// Subclass of Pad.
 dEdXTool.prototype = new Pad(null);           
 
 gUserTrack = new UserTrack();
@@ -127,14 +151,14 @@ function dEdXTool( element, options  )
     log_y : false,
     draw_grid_y : true,
     draw_grid_x : false,
-    margin_left : 30,
-    margin_bottom : 40,
+    margin_left : 50,
+    margin_bottom : 30,
     draw_box : false,    
     margin_right : 10,
     margin_top : 10,
-    xlabel : "Wire",
-    ylabel : "Charge",
-    
+    xlabel : "Wire Num",
+    ylabel : "Hit Charge (Collection Plane)",
+    label_font : "10px sans-serif",    
   };
   // override defaults with options.
   $.extend(true,defaults,options);
@@ -176,9 +200,11 @@ dEdXTool.prototype.Draw = function()
   var nsegments = gUserTrack.points.length-1;
   console.warn("nsegments",nsegments);
   for(var p=0;p<3;p++) {
-    var nbins = Math.ceil(gUserTrack.points[nsegments][p] - gUserTrack.points[0][p]);
-    if(p==2) console.warn("binning:", gUserTrack.points[nsegments][p] , gUserTrack.points[0][p], nbins);
-    this.hists[p] = new Histogram(nbins+1,0,nbins+1);
+    var w2 = Math.ceil(gUserTrack.points[nsegments][p])-1;
+    var w1 = Math.ceil(gUserTrack.points[0][p]);
+    var nbins = Math.abs(w2-w1);
+    console.warn("binning:", w1,w2, nbins);
+    this.hists[p] = new Histogram(nbins,w1,w2);
     this.starts[p] = gUserTrack.points[0][p];
   }
    
@@ -205,7 +231,7 @@ dEdXTool.prototype.Draw = function()
       if(h.t2 < t-r) continue; // hit is too early
       if(h.t1 > t+r) continue; // hit is too late.
       // throw it on the pile!
-      this.hists[p].Fill(w-this.starts[p],h.q);
+      this.hists[p].Fill(w,h.q);
       n++;
     }
   }
@@ -221,6 +247,7 @@ dEdXTool.prototype.Draw = function()
   
   this.Clear();
   this.DrawFrame();
+  this.DrawReferenceCurves();
 
   // Width of a single vertical histogram bar.
   var barwidth = (hist.max-hist.min)/(hist.n)*this.span_x/(this.max_u-this.min_u) ;
@@ -244,4 +271,50 @@ dEdXTool.prototype.Draw = function()
   }
 }
 
+dEdXTool.prototype.MuonCurve = function(x,cosz,top)
+{
+  var par0 =  cosz;
+  var par1 =  6.68361e+01/cosz;
+  var par2 = -9.82047e+01;
+  var par3 = -6.05256e-05;
+  var par4 =  5.76619e-07;
+  var par5 =  3.37519e+01;
 
+  if(top) {
+    par0 =  cosz;
+    par1 =  1.36442e+02/cosz;
+    par2 = -2.08950e+01;
+    par3 =  1.40347e-03;
+    par4 = -4.88850e-07;
+    par5 =  2.10392e+01;	    
+  }
+  
+  var f= par1*(1+par2/(1+par0*x)+par3*par0*x+par4*par0*x*par0*x+par5/Math.log(1+par0*x));
+  // console.log(x,f,cosz);
+  return f;
+}
+
+
+dEdXTool.prototype.DrawReferenceCurves = function()
+{
+  var hist = this.hists[2]; // Just do induction plane for now.
+  
+  // Estimate theta from the track.
+  var cosz = gUserTrack.get_costheta_z(hist.GetX(0)); // FIXME: do per-wire or per-segment
+
+  this.ctx.fillStyle = "rgba(0,0,255,0.5)";
+  this.ctx.beginPath();
+  this.ctx.moveTo( this.GetX(this.min_u), this.GetY( this.MuonCurve(hist.n-1,cosz,true)) );
+
+  for(var i=0;i<hist.n;i++) {
+    var x = hist.n-i;
+    this.ctx.lineTo( this.GetX( hist.GetX(i) ), this.GetY( this.MuonCurve(x,cosz,true)) );
+  }
+  for(var i=hist.n-1;i>=0;i--) {
+    var x = hist.n-i;
+    this.ctx.lineTo( this.GetX( hist.GetX(i) ), this.GetY( this.MuonCurve(x,cosz,false)) );
+  }
+  this.ctx.closePath();
+  this.ctx.fill();
+  
+}
