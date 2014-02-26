1f9cbecd3ba4b1d046ebb818ff763ead5c7dd08e
Nathaniel Tagg <tagg@fnal.gov>
2014-02-26 14:23:10 -0600

    Make the raw backend work again, on the DAQ cluster.
    Former-commit-id: caf27d17082754f6b01cdb6cc49a90a7fb7e5d2f

--- backend/setup.sh
+++ backend/setup.sh
@@ -5,4 +5,4 @@ setup root v5_34_05 -q e2:debug
 setup boost v1_53_0 -q e2:debug
 setup postgres
 setup uboonedaq_datatypes v1_00_02 -q e2:debug
-setup uboonedaq v1_00_02 -q e2:debug
+#setup uboonedaq v1_00_02 -q e2:debug
