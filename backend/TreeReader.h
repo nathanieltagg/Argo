f31374e41ae2663d18c3b725e888e738ab0b15ea
Nathaniel Tagg <ntagg@otterbein.edu>
2014-03-19 07:59:54 -0400

    Fix up code to work without gcc11 at yale.
    Former-commit-id: 1358cf29f5efb6903196a9b214f341d79b194723

--- backend/TreeReader.h
+++ backend/TreeReader.h
@@ -50,27 +50,27 @@ public:
   JsonElement jsonF(const std::string& formula, int index = 0);
 
 
-  
-  // Syntactic sugar: makeArray("key1","formula1","key2","formula2",...)
-  template <class ...B> 
-    JsonArray makeArray(const std::string& k, const std::string& f, B... argTail);
-
-  // For the syntactic sugar to work.
-  template <class ...B> 
-      JsonArray makeArray(std::vector<std::pair< std::string,std::string> >& key_leaf_pairs, 
-                               const std::string& k, const std::string& f,  B... argTail);
-
-
-  
-  // Syntactic sugar: makeArray("key1","formula1","key2","formula2",...)
-  template <class ...B> 
-    JsonArray makeFArray(const std::string& k, const std::string& f, B... argTail);
-
-  // For the syntactic sugar to work.
-  template <class ...B> 
-      JsonArray makeFArray(std::vector<std::pair< std::string,std::string> >& key_formula_pairs, 
-                               const std::string& k, const std::string& f,  B... argTail);
-    
+  // 
+  // // Syntactic sugar: makeArray("key1","formula1","key2","formula2",...)
+  // template <class ...B> 
+  //   JsonArray makeArray(const std::string& k, const std::string& f, B... argTail);
+  // 
+  // // For the syntactic sugar to work.
+  // template <class ...B> 
+  //     JsonArray makeArray(std::vector<std::pair< std::string,std::string> >& key_leaf_pairs, 
+  //                              const std::string& k, const std::string& f,  B... argTail);
+  // 
+  // 
+  // 
+  // // Syntactic sugar: makeArray("key1","formula1","key2","formula2",...)
+  // template <class ...B> 
+  //   JsonArray makeFArray(const std::string& k, const std::string& f, B... argTail);
+  // 
+  // // For the syntactic sugar to work.
+  // template <class ...B> 
+  //     JsonArray makeFArray(std::vector<std::pair< std::string,std::string> >& key_formula_pairs, 
+  //                              const std::string& k, const std::string& f,  B... argTail);
+  //   
 
   // Obsoleted.
   // XmlElement  getXml(const std::string& tagname, const std::string& leafname, int index = 0, int second_index = -1);
@@ -86,39 +86,39 @@ public:
 
 
 ///////////////////////////
-
-
-template <class ...B> 
-JsonArray TreeReader::makeArray(const std::string& k, const std::string& f, B... argTail)
-{
-  std::vector<std::pair< std::string,std::string> > v;
-  return makeArray(v,k,f,argTail...);
-}
-
-template <class ...B> 
-JsonArray TreeReader::makeArray(std::vector<std::pair< std::string,std::string> >& key_leaf_pairs, 
-                                const std::string& k, const std::string& f,  B... argTail)
-{
-  key_leaf_pairs.push_back(make_pair(k,f));
-  return makeArray(key_leaf_pairs,argTail...);
-}
-
-///////////////////////////
-
-template <class ...B> 
-JsonArray TreeReader::makeFArray(const std::string& k, const std::string& f, B... argTail)
-{
-  std::vector<std::pair< std::string,std::string> > v;
-  return makeFArray(v,k,f,argTail...);
-}
-
-template <class ...B> 
-JsonArray TreeReader::makeFArray(std::vector<std::pair< std::string,std::string> >& key_formula_pairs, 
-                                const std::string& k, const std::string& f,  B... argTail)
-{
-  key_formula_pairs.push_back(make_pair(k,f));
-  return makeFArray(key_formula_pairs,argTail...);
-}
+// 
+// 
+// template <class ...B> 
+// JsonArray TreeReader::makeArray(const std::string& k, const std::string& f, B... argTail)
+// {
+//   std::vector<std::pair< std::string,std::string> > v;
+//   return makeArray(v,k,f,argTail...);
+// }
+// 
+// template <class ...B> 
+// JsonArray TreeReader::makeArray(std::vector<std::pair< std::string,std::string> >& key_leaf_pairs, 
+//                                 const std::string& k, const std::string& f,  B... argTail)
+// {
+//   key_leaf_pairs.push_back(make_pair(k,f));
+//   return makeArray(key_leaf_pairs,argTail...);
+// }
+// 
+// ///////////////////////////
+// 
+// template <class ...B> 
+// JsonArray TreeReader::makeFArray(const std::string& k, const std::string& f, B... argTail)
+// {
+//   std::vector<std::pair< std::string,std::string> > v;
+//   return makeFArray(v,k,f,argTail...);
+// }
+// 
+// template <class ...B> 
+// JsonArray TreeReader::makeFArray(std::vector<std::pair< std::string,std::string> >& key_formula_pairs, 
+//                                 const std::string& k, const std::string& f,  B... argTail)
+// {
+//   key_formula_pairs.push_back(make_pair(k,f));
+//   return makeFArray(key_formula_pairs,argTail...);
+// }
 
 
 
