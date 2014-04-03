da5043ff7573eca1a374fcf659fde55e452d0ef9
Nathaniel Tagg <tagg@fnal.gov>
2014-04-03 16:51:27 -0500

    Minor stability edits.

--- server/file_browser_config.pl
+++ server/file_browser_config.pl
@@ -2,4 +2,5 @@ $title =  "Argo File Browser";
 $default_path = "/uboone";
 $cookie_name = 'argo_file_browser';
 $link_target = "../";
+$restrict_to = [ getcwd(), "/uboone","/minos","/minerva", "/Users/tagg"];
 1;
