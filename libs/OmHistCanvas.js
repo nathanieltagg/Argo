cf095582627b2e216a72c9f44b2d158c0b558e33
Nathaniel Tagg <tagg@fnal.gov>
2014-07-21 10:27:17 -0500

    Mods to make it work properly on near1
    Former-commit-id: ba7a414719321b6688913e03e4cb49f50125be30

--- libs/OmHistCanvas.js
+++ libs/OmHistCanvas.js
@@ -104,10 +104,6 @@ OmHistCanvas.prototype.Update = function()
   this.time_on_x   = this.hist.time_on_x;
   
   if((this.controls_init == false) && (this.hist.info)) {
-    
-    
-    blahhhhh! 
-    
     if("draw_fill"          in this.hist.info) $(this.ctl_histo_fill).prop('checked',this.hist.info.draw_fill);
     if("draw_logy"          in this.hist.info) $(this.ctl_histo_logscale).prop('checked',this.hist.info.draw_logy);
     if("draw_zero_suppress" in this.hist.info)  this.zero_suppress = this.hist.info.draw_zero_suppress;
