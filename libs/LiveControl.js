fa377705810b18920ffc9d590daf53ae0ce145d2
Nathaniel Tagg <tagg@fnal.gov>
2015-03-06 12:39:17 -0500

    Add some cleverness making a clock picture.
    Former-commit-id: 5c62f5dd99238d4809d02e0c868322afd11eae26

--- libs/LiveControl.js
+++ libs/LiveControl.js
@@ -7,6 +7,8 @@ $(function(){
   });  
 });
 
+
+
 function LiveControl( element )
 {
   this.element = element;
@@ -85,3 +87,30 @@ LiveControl.prototype.NewRecord = function()
   this.recent_cache_file = gServing.live_cache_file;
   if(gServing.live_cache_file  > this.latest_cache_file) this.latest_cache_file = gServing.live_cache_file;
 }
+
+
+
+function clockface(elem,frac) {
+  // CSS cleverness.  Take 'elem' as object or as jquery specifyier.  Take 'frac' as fraction filled.
+  // Element must have width,height set (as equal usually). You can change color by setting background-color and color on the object by css or style.
+  var e = $(elem);
+  var color1 = e.css('background-color');
+  var color2 = e.css('color');
+  // var color2 = "black";
+  $(elem).css('border-radius','50%');
+  // $(elem).css('border','1px solid '+color2);
+  if(frac<0.5) {
+    var angle2 = (frac*360 + 90);
+    angle2 += 'deg';
+    console.log('less',angle2);
+    e.css('background-image',
+    'linear-gradient(90deg,'+color1+'50%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0)),linear-gradient('+angle2+','+color2+' 50% ,'+color1+' 50%, '+color1+')');
+  } else {
+    var angle1 = Math.round((frac-0.5)*360 - 90);
+    angle1 += 'deg';
+    console.log('more',angle1);
+    e.css('background-image','linear-gradient('+angle1+','+color2+' 50%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0)),linear-gradient(270deg,'+color2+' 50% ,'+color1+' 50%, '+color1+')');
+   }
+  
+}
+
