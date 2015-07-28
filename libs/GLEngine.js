81c3bad9f3e63b26ed11f84f98bd573032522196
Nathaniel Tagg <tagg@fnal.gov>
2015-07-28 14:09:21 -0400

    Change rendering engine.

--- libs/GLEngine.js
+++ libs/GLEngine.js
@@ -1,45 +1,417 @@
 
+
+$(function(){
+  gGLMapperRaw = new GLMapper('raw');
+  gGLMapperCal = new GLMapper('cal');
+
+  // gStateMachine.Bind('ChangePsuedoColor',function(){
+  //   this.show_image = $(this.ctl_wireimg_type).filter(":checked").val() || "raw";
+  //   var objnm = '_' + this.show_image;
+  //   UpdateColoredWireMap(objnm);
+  // });
+});
+
+function GLMapper(typ) // "raw" or "cal"
+{
+  this.typ = typ; 
+  
+  // Create openGL engine.
+  this.tilesize = 2048; // Will be overwritten later.
+  this.renderCanvas = document.createElement('canvas');
+  this.renderCanvas.width = this.tilesize;
+  this.renderCanvas.height = this.tilesize;
+
+  
+  console.time("GLEngine::create gl context");
+  try {
+    this.gl = this.renderCanvas.getContext('webgl',{ alpha: false, antialias: false,  depth: false });    
+  } catch(e) 
+  {
+    console.error("Cannot crate WebGL context: "  + e.toString());
+    return;
+  }
+  console.timeEnd("GLEngine::create gl context");
+  if(!this.gl) {
+    console.error("Lost GL context somewhere.");
+  }
+    
+   // This line is required if we change canvas size
+  // after creating GL context
+  // this.gl.viewport(0,0,outcanvas.width,outcanvas.height);
+  
+  // setup GLSL this.program
+  var vertexShader = this.create_shader( "2d-vertex-shader");
+  var LUTShader    = this.create_shader( "lutshade");
+
+  this.program = this.gl.createProgram();
+  this.gl.attachShader(this.program,vertexShader);
+  this.gl.attachShader(this.program,LUTShader);
+  this.gl.linkProgram(this.program);
+
+  // Check the link status
+  var linked = this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS);
+  if (!linked) {
+       // something went wrong with the link
+       lastError = this.gl.getProgramInfoLog (this.program);
+       console.error("Error in this.program linking:" + lastError);
+
+       this.gl.deleteProgram(this.program);
+       return null;
+   }
+
+
+  this.gl.useProgram(this.program);
+
+
+  // provide texture coordinates for the rectangle we're drawing FROM
+  var texCoordBuffer = this.gl.createBuffer();
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, texCoordBuffer);
+  this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+      0.0,  0.0,
+      1.0,  0.0,
+      0.0,  1.0,
+      0.0,  1.0,
+      1.0,  0.0,
+      1.0,  1.0]), this.gl.STATIC_DRAW);
+  var texCoordLocation = this.gl.getAttribLocation(this.program, "a_texCoord");  // Ditto a_texCoord
+  this.gl.enableVertexAttribArray(texCoordLocation);
+  this.gl.vertexAttribPointer(texCoordLocation, 2, this.gl.FLOAT, false, 0, 0);
+
+  // Space to hold image tiles.
+  this.tile_images = [];
+  this.loaded = false;
+  var total_width = 0;
+  var total_height = 0;
+  this.num_images_loaded = 0;
+  this.num_images_needed = 0;
+  
+  
+  // Space to hold textures.
+  this.tile_textures = [];
+
+  gStateMachine.Bind('recordChange',this.NewRecord.bind(this));
+  gStateMachine.Bind('ChangePsuedoColor',this.Render.bind(this));
+
+}
+
+GLMapper.prototype.NewRecord = function()
+{
+  if(!gRecord) return;
+  
+  
+  for(product in gRecord[this.typ]) {
+    // Create a tiled image to hold this raw data.
+    if(gRecord[this.typ][product]) {
+      this.tile_urls= gRecord[this.typ][product].wireimg_encoded_tiles;
+      if(this.tile_urls) this.StartLoad();
+    }    
+  }
+}
+
+GLMapper.prototype.StartLoad = function()
+{
+  // FIXME Could explicitly delete images and textures - might improve GPU memory, but not required.
+  this.tile_images = [];
+  this.tile_textures = [];
+  
+  console.time("TiledImageCanvas.begin_loading");
+  var self = this;
+  
+  this.total_width = 0;
+  this.total_height = 0;
+  this.num_images_loaded = 0;
+  this.num_images_needed = 0;
+
+  for(var irow=0;irow<this.tile_urls.length;irow++) {
+    var row_width = 0;
+    var row_height = 0;
+    var row = this.tile_urls[irow];
+    var imagerow = [];
+    var texturerow = [];
+    for(var icol=0;icol<row.length;icol++) {
+      this.num_images_needed++
+      var elem = this.tile_urls[irow][icol];
+      var image_url = elem.url;
+      var img = new Image();
+      img.src = image_url;
+      imagerow.push(img);
+      texturerow.push(this.gl.createTexture());
+      (function(){  // Make a closure to copy the values of irow and icol
+        var jrow = irow;
+        var jcol = icol;
+        console.log("setting callback for",jrow,jcol);        
+        img.onload= function() { self.ImageLoaded(jrow,jcol); }
+      })();
+      row_height = Math.max(elem.height,row_height);
+      row_width  += elem.width;
+    }
+    this.total_height += row_height;
+    this.total_width  = Math.max(row_width,this.total_width);
+    this.tile_images.push(imagerow);
+    this.tile_textures.push(texturerow);
+  }
+}
+
+GLMapper.prototype.ImageLoaded = function(jrow,jcol)
+{
+  console.time("GLMapper::ImageLoaded: one image");
+  //Draw in this particular item.
+  var elem = this.tile_urls[jrow][jcol];
+  var img = this.tile_images[jrow][jcol];
+  var tex = this.tile_textures[jrow][jcol];
+
+  // Load the texture...
+  
+  this.gl.activeTexture(this.gl.TEXTURE0); // Set active unit
+  this.gl.bindTexture(this.gl.TEXTURE_2D,tex); // bind our texture to the unit.
+  
+  // Always use nearest pixel, NO INTERPOLATION:
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+  // Always use nearest pixe, EVEN IF OVER THE EDGE (also solves non-power-of-two)
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+  this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, img);
+
+  console.timeEnd("GLMapper::ImageLoaded: one image");
+  this.num_images_loaded++;
+    
+  // See if we have them all...
+  var loaded = true;
+  for(var irow=0;irow<this.tile_images.length;irow++) {
+    for(var icol=0;icol<this.tile_images[irow].length;icol++) {
+      var elem = this.tile_urls[irow][icol];
+      var img = this.tile_images[irow][icol];
+      if( !(img.complete) ) loaded = false;
+    }
+  }
+  this.loaded = loaded;
+  if(!loaded) return;
+
+  console.log("TiledImageCanvas loaded",this.name);
+  this.Render();
+}
+
+GLMapper.prototype.build_LUT_texture = function( ) 
+{ 
+  
+  // Creates an OpenGl texture, returns the texture ID.
+  // This version builds a 2d 256x256 texture.
+  // Colors go top-to-bottom (most sigificant changes) and left-to-right (least significant)
+  // This forms a full 256x256 lookup table usable by the shader.
+  // Note that range of values accessible is only -4096 to 4096, (-0x1000 to 0x1000), so only needs 0x2000 values out of 0x10000 pixels
+  // in a 256x256 image. So, only fills 1/8 of image space. Need to push it up 
+  // I _think_ that this would work with smaller resolution, but color changes at small ADC value wont' be visable.
+  var canvas = document.createElement("canvas");
+  canvas.width  = 256;
+  canvas.height = 256;
+  var start_x = -0x1000;
+  var stop_x =   0x1000
+  var pixels = 0x2000; // Total pixels possible from -4096 to 4096
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0,256,256);
+  var imgData=ctx.createImageData(256,32); // 256*16 = 8192 = 0x2000 possible values.
+  var len = imgData.data.length;
+  for (var i=0;i<len;i+=4) {
+    var x = start_x + (i/4.)*(stop_x-start_x)/pixels; 
+    var color = gWirePseudoColor.interpolate(x);      
+    imgData.data[i+0]= color.r;
+    imgData.data[i+1]= color.g;
+    imgData.data[i+2]= color.b;
+    imgData.data[i+3]= color.a;
+  }
+  ctx.putImageData(imgData,0,112); // Shift up 7/16ths to center it correctly.
+
+    
+  // Creates an OpenGl texture, returns the texture ID.
+  if(this.LUT_texture)  this.gl.deleteTexture(this.LUT_texture);
+  this.LUT_texture = this.gl.createTexture();
+  this.gl.activeTexture(this.gl.TEXTURE7); // Set active unit to 1
+  this.gl.bindTexture(this.gl.TEXTURE_2D, this.LUT_texture);
+
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+  this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, canvas);
+}
+
+GLMapper.prototype.Render = function()
+{
+  console.log('GLMapper::Render');
+  console.time('GLMapper::Render');
+
+  // copy the output canvas to the gRecord so others can get at it.
+  var utyp = '_' + this.typ;
+  if(!gRecord[utyp]) gRecord[utyp] = {};
+  if(!gRecord[utyp].colored_wire_canvas) gRecord[utyp].colored_wire_canvas = this.renderCanvas;  
+
+  this.renderCanvas.width  = this.total_width;
+  this.renderCanvas.height = this.total_height;
+
+
+  var gl = this.gl; // for convenience
+
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(this.program, "a_position");  // Get a pointer to the a_position input given to the vertex shader fragment in the this.program.
+
+  console.time('Build LUT');  
+  var mapTextureLocation = gl.getUniformLocation(this.program, "maptexture");
+  this.build_LUT_texture();
+  gl.uniform1i(mapTextureLocation, 7);
+  gl.bindTexture(gl.TEXTURE_2D, this.LUT_texture);
+  console.timeEnd('Build LUT');
+
+  // lookup uniforms
+  var resolutionLocation = gl.getUniformLocation(this.program, "u_resolution");
+  
+  var inputTextureLocation = gl.getUniformLocation(this.program, "texture");      
+  gl.uniform1i(inputTextureLocation, 1); // use TEXTURE1 as your input!
+  
+  var resolutionLocation = gl.getUniformLocation(this.program, "u_resolution");
+  
+  // loop textures.
+  for(var irow=0;irow<this.tile_textures.length;irow++) {
+    for(var icol=0;icol<this.tile_textures[irow].length;icol++) {
+      var elem = this.tile_urls[irow][icol];
+      var tex = this.tile_textures[irow][icol];
+      var x = elem.x;
+      var y = elem.y;
+      var w = elem.width;
+      var h = elem.height;
+      gl.uniform2f(resolutionLocation, elem.width, elem.height);  
+      
+      this.gl.viewport(elem.x, elem.y, elem.width, elem.height);
+      gl.activeTexture(gl.TEXTURE1);        
+      gl.bindTexture(gl.TEXTURE_2D, tex);
+      gl.uniform2f(resolutionLocation, elem.width, elem.height);
+      this.RenderToRect(0,0,elem.width,elem.height,positionLocation);
+      
+      
+    }
+  }
+  
+  console.timeEnd('GLMapper::Render');
+  console.log("Time to finish drawing via full-canvas:", performance.now() - gTimeStats_RecordChange);
+  console.log("Time from start of query:", performance.now() - gTimeStats_StartQuery);
+  gStateMachine.Trigger('colorWireMapsChanged');
+}
+
+
+GLMapper.prototype.RenderToRect = function( x, y, width, height, positionLocation ) 
+{
+  // Create a buffer for the position of the rectangle corners we're drawing INTO.
+  var buffer = this.gl.createBuffer();
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+  this.gl.enableVertexAttribArray(positionLocation);
+  this.gl.vertexAttribPointer(positionLocation, 2, this.gl.FLOAT, false, 0, 0);
+  // Set a rectangle the same size as the image.
+  // setRectangle(gl, 0, 0, image.width, image.height);
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2]), this.gl.STATIC_DRAW);
+
+  // Draw the rectangle.
+  this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
+
+  // Clean up
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+  this.gl.deleteBuffer(buffer);
+}
+
+
+GLMapper.prototype.create_shader = function( scriptId  )
+{
+  var shaderSource = "";
+  var shaderType;
+  var shaderScript = document.getElementById(scriptId);
+  if (!shaderScript) {
+    throw("*** Error: unknown script element" + scriptId);
+  }
+
+  shaderSource = shaderScript.text;
+  if (shaderScript.type == "x-shader/x-vertex") {
+    shaderType = this.gl.VERTEX_SHADER;
+  } else if (shaderScript.type == "x-shader/x-fragment") {
+    shaderType = this.gl.FRAGMENT_SHADER;
+  } else if (shaderType != this.gl.VERTEX_SHADER && shaderType != this.gl.FRAGMENT_SHADER) {
+    throw("*** Error: unknown shader type");
+    return null;
+  }
+
+   // Create the shader object
+   var shader = this.gl.createShader(shaderType);
+
+   // Load the shader source
+   this.gl.shaderSource(shader, shaderSource);
+
+   // Compile the shader
+   this.gl.compileShader(shader);
+
+   // Check the compile status
+   var compiled = this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS);
+
+   if (!compiled) {
+     // Something went wrong during compilation; get the error
+     lastError = this.gl.getShaderInfoLog(shader);
+     console.error("*** Error compiling shader ",shader, ":" ,lastError);
+     this.gl.deleteShader(shader);
+     return null;
+   }
+
+   return shader;
+} 
+
 //
 // Code to handle 2d openGL manipulations.
 //
 
-$(function(){
-  gGLEngine = new GLEngine();
-  gStateMachine.Bind('TiledImageLoaded_raw',function(){ UpdateColoredWireMap("_raw"); });
-  gStateMachine.Bind('TiledImageLoaded_cal',function(){ UpdateColoredWireMap("_cal"); });
-
-  gStateMachine.Bind('ChangePsuedoColor',function(){ 
-    this.show_image = $(this.ctl_wireimg_type).filter(":checked").val() || "raw";
-    var objnm = '_' + this.show_image;
-    UpdateColoredWireMap(objnm); 
-  });
-});
-
-function UpdateColoredWireMap(typ)
-{
-  
-  // console.log("UpdateColoredWireMap",typ);
-  // Create the LUT-translated colored wiremaps images.
-  if(!gRecord) return;
-  // FIXME: assumes only 1 type of raw or cal wires in any given loaded event.
-  if(gRecord[typ] && gRecord[typ].tiled_canvas) {
-    gRecord[typ].colored_wire_canvas = document.createElement('canvas');
-    gRecord[typ].colored_wire_canvas.width = gRecord[typ].tiled_canvas.canvas.width;
-    gRecord[typ].colored_wire_canvas.height = gRecord[typ].tiled_canvas.canvas.height;
-    // Does this help the aw snaps? No, no it doesn't.
-    // gGLEngine = null;
-    // gGLEngine = new GLEngine();
-    gGLEngine.draw_falsecolor_from_canvas(
-                       gRecord[typ].tiled_canvas.canvas,
-                       gRecord[typ].colored_wire_canvas,
-                       gWirePseudoColor 
-      );
-  }
-  gStateMachine.Trigger('colorWireMapsChanged');
-  
-};
-
-
+// $(function(){
+//   gGLEngine = new GLEngine();
+//   gStateMachine.Bind('TiledImageLoaded_raw',function(){ UpdateColoredWireMap("_raw"); });
+//   gStateMachine.Bind('TiledImageLoaded_cal',function(){ UpdateColoredWireMap("_cal"); });
+//
+//   gStateMachine.Bind('ChangePsuedoColor',function(){
+//     this.show_image = $(this.ctl_wireimg_type).filter(":checked").val() || "raw";
+//     var objnm = '_' + this.show_image;
+//     UpdateColoredWireMap(objnm);
+//   });
+// });
+//
+// function UpdateColoredWireMap(typ)
+// {
+//
+//   // console.log("UpdateColoredWireMap",typ);
+//   // Create the LUT-translated colored wiremaps images.
+//   if(!gRecord) return;
+//   // FIXME: assumes only 1 type of raw or cal wires in any given loaded event.
+//   if(gRecord[typ] && gRecord[typ].tiled_canvas) {
+//     gRecord[typ].colored_wire_canvas = document.createElement('canvas');
+//     gRecord[typ].colored_wire_canvas.width = gRecord[typ].tiled_canvas.canvas.width;
+//     gRecord[typ].colored_wire_canvas.height = gRecord[typ].tiled_canvas.canvas.height;
+//     // Does this help the aw snaps? No, no it doesn't.
+//     // gGLEngine = null;
+//     // gGLEngine = new GLEngine();
+//     gGLEngine.draw_falsecolor_from_canvas(
+//                        gRecord[typ].tiled_canvas.canvas,
+//                        gRecord[typ].colored_wire_canvas,
+//                        gWirePseudoColor
+//       );
+//   }
+//   gStateMachine.Trigger('colorWireMapsChanged');
+//
+// };
+//
+//
 
 
 
