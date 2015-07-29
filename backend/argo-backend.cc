e747ff14411a337fcbd75d0bfa3ab83501279c53
Nathaniel Tagg <tagg@fnal.gov>
2015-07-29 13:49:11 -0500

    Fix build. Add plex3
    Former-commit-id: 36ced18e15e2a31a4e74fd036221d92b49719643

--- backend/argo-backend.cc
+++ backend/argo-backend.cc
@@ -129,7 +129,7 @@ int main(int argc, char **argv)
 
 
     // Plexus.
-    gPlexus.build( "sqlite", "../config/plexus2.db" );
+    gPlexus.build( "sqlite", "../config/plexus3.db" );
     if(!gPlexus.is_ok()) {
       cout << "Can't configure plexus from sqlite." << endl;
       gPlexus.build("postgresql","host=fnalpgsdev.fnal.gov port=5436 dbname=uboonedaq_dev user=uboonedaq_web password=argon!uBooNE");
