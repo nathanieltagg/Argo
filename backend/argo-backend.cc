a4004f99076ef4498d4cb0d34c20f71a0089717d
Nathaniel Tagg <tagg@fnal.gov>
2015-08-17 15:24:22 -0500

    Move things to db directory
    Former-commit-id: 7e6941cbbad3d33cccd71e952bca933ddea8a12b

--- backend/argo-backend.cc
+++ backend/argo-backend.cc
@@ -129,9 +129,9 @@ int main(int argc, char **argv)
 
 
     // Plexus.
-    gPlexus.build( "sqlite", "../config/current_plexus.db" );
+    gPlexus.build( "sqlite", "../db/current-plexus.db" );
     if(!gPlexus.is_ok()) {
-      cout << "Can't configure plexus from sqlite: ../config/current_plexus.db" << endl;
+      cout << "Can't configure plexus from sqlite: ../db/current-plexus.db" << endl;
       gPlexus.build("postgresql","host=fnalpgsdev.fnal.gov port=5436 dbname=uboonedaq_dev user=uboonedaq_web password=argon!uBooNE");
     }
     if(!gPlexus.is_ok()) {
