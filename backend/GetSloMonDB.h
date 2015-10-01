565f3fa463c30a51084ec8a39d59f495aa6c26f2
Nathaniel Tagg <tagg@ubooneevd1.fnal.gov>
2015-10-01 16:28:32 -0500

    Make it compile on linux

--- backend/GetSloMonDB.h
+++ backend/GetSloMonDB.h
@@ -10,20 +10,8 @@
 class GetSlowMonDB
 {
 public:
-  GetSlowMonDB(const std::string& inTime="now", const std::string& inChannel="uB_TPCDrift_HV01_1_0/voltage")
-      : val("{\"error\":\"Can't connect to DB\"}")
-      , time(inTime)
-      , channel(inChannel)
-        {};
-    GetSlowMonDB(double inTime, const std::string& inChannel="uB_TPCDrift_HV01_1_0/voltage")
-          : val("{\"error\":\"Can't connect to DB\"}")
-          , channel(inChannel)
-            {
-              std::time_t t = inTime;
-              std::ostringstream os;
-              os << std::put_time(std::gmtime(&t),"%F %T"); 
-              time = os.str();
-            };
+  GetSlowMonDB(const std::string& inTime="now", const std::string& inChannel="uB_TPCDrift_HV01_1_0/voltage");
+  GetSlowMonDB(double inTime, const std::string& inChannel="uB_TPCDrift_HV01_1_0/voltage");
 
   void operator()();
   
