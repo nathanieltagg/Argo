ebc0285b5e5f3e8a574999b11607d110b250b1fe
Nathaniel Tagg <tagg@fnal.gov>
2016-04-29 08:59:39 -0500

    Putting in 'screw this' message.
    Former-commit-id: 88de82b5183964bfff8485e534c575a35bb93553

--- backend/LinkDef.h
+++ backend/LinkDef.h
@@ -1,14 +1,14 @@
 // This file contains the instructions that tell rootcint how to 
 // build the Dictionary.cxx file.
-
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ operators vector<vector<ULong64_t>>::iterator;
-#pragma link C++ operators vector<vector<ULong64_t>>::const_iterator;
-#pragma link C++ operators vector<vector<ULong64_t>>::reverse_iterator;
+#pragma link C++ operators vector<vector<unsigned int>>+;
+#pragma link C++ operators vector<vector<unsigned int>>::iterator;
+#pragma link C++ operators vector<vector<unsigned int>>::const_iterator;
+#pragma link C++ operators vector<vector<unsigned int>>::reverse_iterator;
 
 
 #endif
