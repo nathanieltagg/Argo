436b17fc2b805bd97340ec7fcea423b5a1ed1f69
Nathaniel Tagg <ntagg@otterbein.edu>
2018-06-26 09:56:02 -0500

    Local mods

--- server/file_browser_config.pl
+++ server/file_browser_config.pl
@@ -2,7 +2,7 @@ $title =  "Argo File Browser";
 $default_path = "/uboone";
 $cookie_name = 'argo_file_browser';
 $link_target = "../";
-$restrict_to = [ getcwd(), "/uboone","/minos","/minerva", "/Users/tagg", "/pnfs", "/home/argo" ];
+$restrict_to = [ getcwd(), "/uboone","/minos","/minerva", "/Users/tagg", "/pnfs/", "/home/argo", "/localdata/" ];
 $quick_links = [ "/uboone/app", "/uboone/data", "/pnfs/uboone/scratch", "/pnfs/uboone/persistent", "/pnfs/uboone/data", ];
 
 1;
